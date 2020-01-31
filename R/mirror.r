6acb7759cee9b69175c1488e4366c84d52807e63
Adam B. Smith <adam@earthskysea.org>
2020-01-31 11:08:45 -0600

    mirror() now handles matrices and data frames

--- R/mirror.r
+++ R/mirror.r
@@ -1,6 +1,6 @@
-#' Reverse order of characters in a string or digits in a number
+#' Mirror image of a value
 #'
-#' This function creates a "mirror" image of a character string or a number. For example "Shots were exchanged at the hospital" becomes "latipsoh eht ta degnahcxe erew stohS' and 3.14159 becomes 95141.3.
+#' This function creates a "mirror" image of a character string, a number, a matrix, or a data frame. For example "Shots were exchanged at the hospital" becomes "latipsoh eht ta degnahcxe erew stohS' and 3.14159 becomes 95141.3. Data frames and matrices will be returned with the order of columsnn reversed.
 #' @param x Numeric or character or a vector of numeric or character values.
 #' @returns Object with same class as \code{x}.
 #' @examples
@@ -13,6 +13,12 @@
 #' # last value will return NA because the exponentiation does not 
 #' # make sense when written backwards
 #' x <- c(3.14159, 2.71828, 6.02214076e+23)
+#'
+#' mirror(x)
+#' x <- data.frame(x=1:5, y=6:10)
+#' mirror(x)
+#'
+#' x <- matrix(1:10, nrow=2)
 #' mirror(x)
 #' export
 mirror <- function(x) {
@@ -23,10 +29,18 @@ mirror <- function(x) {
 		x <- as.character(x)
 	}
 
-	x <- strsplit(x, '')
-	x <- lapply(x, rev)
-	x <- sapply(x, paste, collapse='')
+	if (xClass %in% c('character', 'numeric', 'integer', '64bit')) {
+		
+		x <- strsplit(x, '')
+		x <- lapply(x, rev)
+		x <- sapply(x, paste, collapse='')
+		
+	} else if (xClass %in% c('matrix', 'data.frame')) {
+	
+		x <- x[ , ncol(x):1]
 
+	}
+		
 	class(x) <- xClass
 	x
 
