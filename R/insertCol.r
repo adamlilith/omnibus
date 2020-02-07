c913d5c98bcd35bea27d466a3260616bf8777a15
Adam B. Smith <adam@earthskysea.org>
2020-02-07 00:08:27 -0600

    insertCol() now works like cbind() if at is NULL

--- R/insertCol.r
+++ R/insertCol.r
@@ -3,8 +3,8 @@
 #' This function inserts one or more columns before or after another column in a data frame or matrix. It is similar to \code{\link[base]{cbind}} except that the inserted column(s) can be placed anywhere.
 #' @param x Data frame, matrix, or vector with same number of rows or elements as \code{into}.
 #' @param into Data frame or matrix into which \code{x} is to be inserted.
-#' @param at Character or integer, name of column or column number at which to do insertion.
-#' @param before Logical, if \code{TRUE} (default) then the insertion will occur in front of the column named in \code{at}, if \code{FALSE} then after.
+#' @param at Character, integer, or \code{NULL}. Name of column or column number at which to do insertion. If \code{NULL} (default), the result is exactly the same as \code{cbind(into, x} except that it retains row numbers from \code{into}.
+#' @param before Logical, if \code{TRUE} (default) then the insertion will occur in front of the column named in \code{at}, if \code{FALSE} then after. Ignored if \code{at} is \code{NULL}.
 #' @return A data frame.
 #' @seealso \code{\link[base]{merge}}, \code{\link[base]{cbind}}
 #' @examples
@@ -13,11 +13,12 @@
 #'
 #' insertCol(x, into=into, at='x3')
 #' insertCol(x, into=into, at='x3', before=FALSE)
+#' insertCol(x, into)
 #' @export
 insertCol <- function(
 	x,
 	into,
-	at,
+	at = NULL,
 	before=TRUE
 ) {
 
@@ -33,26 +34,37 @@ insertCol <- function(
 		
 	# x has rows
 	} else {
-		
-		intoCols <- ncol(into)
+	
 		rowNames <- row.names(into)
-		
-		x <- if (at == 1 & before) {
-			cbind(x, into)
-		} else if (at == 1 & !before) {
-			cbind(into[ , 1, drop=FALSE], x, into[ , 2:intoCols, drop=FALSE])
-		} else if (at == intoCols) {
-			cbind(into, x)
-		} else if (before) {
-			cbind(into[ , 1:(at - 1)], x, into[ , at:intoCols])
-		} else if (!before) {
-			cbind(into[ , 1:at], x, into[ , (at + 1):intoCols])
+			
+		# at is NULL
+		if (is.null(at)) {
+	
+			into <- cbind(into, x)
+	
+		# x has rows and at is not NULL	
+		} else {
+			
+			intoCols <- ncol(into)
+
+			into <- if (at == 1 & before) {
+				cbind(x, into)
+			} else if (at == 1 & !before) {
+				cbind(into[ , 1, drop=FALSE], x, into[ , 2:intoCols, drop=FALSE])
+			} else if (at == intoCols) {
+				cbind(into, x)
+			} else if (before) {
+				cbind(into[ , 1:(at - 1)], x, into[ , at:intoCols])
+			} else if (!before) {
+				cbind(into[ , 1:at], x, into[ , (at + 1):intoCols])
+			}
+			
 		}
 	  
-		rownames(x) <- rowNames
+		rownames(into) <- rowNames
 		
 	}
 	
-	x
+	into
 
 }
