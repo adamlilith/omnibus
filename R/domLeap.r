21e7eb4e8878f0795c97689191eb86a7d8897f09
Adam Smith <asmith@mobot.org>
2020-03-05 11:40:14 -0600

    Add domLeap data

---
+++ R/domLeap.r
@@ -0,0 +1,20 @@
+#' Day of month for leap years
+#'
+#' Data frame of day of month for each month in a leap year.
+#'
+#' @docType data
+#'
+#' @usage data(doyLeap)
+#'
+#' @format An object of class \code{'data.frame'}.
+#'
+#' @keywords datasets
+#'
+#' @references
+#'
+#' @source
+#'
+#' @examples
+#' data(domLeap)
+#' domLeap
+'domLeap'
