1d653fbbaacd3612b165b13a357f333ae23c3e3a
Adam B. Smith <adam@earthskysea.org>
2020-06-13 22:05:11 -0500

    Change default argument order in lsos()

--- R/lsos.r
+++ R/lsos.r
@@ -1,36 +1,52 @@
 #' Size of objects taking most memory
 #'
 #' Displays the largest objects in memory.
-#' @details Adapted from StackOverflow user Dirk Eddelbuettel (original functon at https://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session).
+#' @param n Positive integer, number of objects to display.
+#' @param pattern Character, return size of objects with this pattern in their name.
+#' @param orderBy Either \code{NULL} (default) or any of c\code{'Type'}, \code{'Size'}, \code{'PrettySize'}, \code{'Rows'}, or \code{'Columns'}. Controls field by which objects are sorted.
+#' @param decreasing Logical, if \code{FALSE} (default), objects are displayed from larges to smallest.
+#' @param pos Integer, indicates environment in which to search for objects. See help for \code{\link{ls}}.
+#' @details Adapted from StackOverflow user Dirk Eddelbuettel (original function at https://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session).
 #' @return Nothing (displays a table).
+#' @examples
+#' lsos()
+#' lsos(3)
 #' @export
 
-.ls.objects <- function (pos = 1, pattern, order.by,
-                        decreasing=FALSE, head=FALSE, n=5) {
-    napply <- function(names, fn) sapply(names, function(x)
-                                         fn(get(x, pos = pos)))
-    names <- ls(pos = pos, pattern = pattern)
-    obj.class <- napply(names, function(x) as.character(class(x))[1])
+.ls.objects <- function (
+	n,
+	pattern,
+	orderBy,
+	decreasing=FALSE,
+	pos = 1
+) {
+
+    napply <- function(names, fn) sapply(names, function(x) fn(get(x, pos=pos)))
+    names <- ls(pos=pos, pattern=pattern)
+    
+	obj.class <- napply(names, function(x) as.character(class(x))[1])
     obj.mode <- napply(names, mode)
     obj.type <- ifelse(is.na(obj.class), obj.mode, obj.class)
     obj.size <- napply(names, object.size)
-    obj.prettysize <- sapply(obj.size, function(r) prettyNum(r, big.mark = ",") )
-    obj.dim <- t(napply(names, function(x)
-                        as.numeric(dim(x))[1:2]))
-    vec <- is.na(obj.dim)[, 1] & (obj.type != "function")
+    obj.prettysize <- sapply(obj.size, function(r) prettyNum(r, big.mark=',') )
+    obj.dim <- t(napply(names, function(x) as.numeric(dim(x))[1:2]))
+    
+	vec <- is.na(obj.dim)[, 1] & (obj.type != 'function')
     obj.dim[vec, 1] <- napply(names, length)[vec]
-    out <- data.frame(obj.type, obj.size,obj.prettysize, obj.dim)
-    names(out) <- c("Type", "Size", "PrettySize", "Rows", "Columns")
-    if (!missing(order.by))
-        out <- out[order(out[[order.by]], decreasing=decreasing), ]
-        out <- out[c("Type", "PrettySize", "Rows", "Columns")]
-        names(out) <- c("Type", "Size", "Rows", "Columns")
-    if (head)
-        out <- head(out, n)
-    out
+    
+	out <- data.frame(obj.type, obj.size,obj.prettysize, obj.dim)
+    names(out) <- c('Type', 'Size', 'PrettySize', 'Rows', 'Columns')
+    
+	if (!is.null(orderBy)) out <- out[order(out[[orderBy]], decreasing=decreasing), ]
+    out <- out[c('Type', 'PrettySize', 'Rows', 'Columns')]
+    names(out) <- c('Type', 'Size', 'Rows', 'Columns')
+    out <- out[1:n, ]
+	
+	out
+
 }
 
 # shorthand
-lsos <- function(..., n=10) {
-    .ls.objects(..., order.by="Size", decreasing=TRUE, head=TRUE, n=n)
+lsos <- function(n=10, ...) {
+    .ls.objects(n=n, ..., orderBy='Size', decreasing=TRUE)
 }
