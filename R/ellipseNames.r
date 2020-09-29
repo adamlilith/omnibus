8a9116f4007f634bf25dd4ce08502d69bb7d382c
Adam B. Smith <adam@earthskysea.org>
2020-09-29 13:51:41 -0500

    De-exported

--- R/ellipseNames.r
+++ R/ellipseNames.r
@@ -3,12 +3,11 @@
 #' This function returns the names of objects input into a function as ellipses. It is only useful if called inside a function.
 #' @param ... Objects.
 #' @return Character list.
-#' @export
 ellipseNames <- function(...) {
 
 	ellipses <- as.list(substitute(list(...)))[-1L]
 	inNames <- rep(NA, length(ellipses))
 	for (i in seq_along(ellipses)) inNames[i] <- as.character(ellipses[[i]])
 	inNames
-	
+
 }
