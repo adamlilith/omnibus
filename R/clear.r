1a84267cf766750d46ce81833e27732213443555
Adam Smith <asmith@mobot.org>
2021-10-30 11:21:49 -0500

    Correct help

--- R/clear.r
+++ R/clear.r
@@ -1,7 +1,7 @@
 #' Erase everything in memory and perform garbage collection.
 #'
 #' This is a one-line function for the code: \code{rm(list=ls()); gc()}.
-#' @param rm Logical, if \code{FALSE} (default), everything will be cleared from memory. If \code{TRUE}, then nothing will be cleared but garbage collection will be implemented.
+#' @param rm Logical, if \code{TRUE} (default), everything will be cleared from memory. If \code{FALSE}, then nothing will be cleared but garbage collection will be implemented.
 #' @return Nothing (clears everything from memory).
 #' @examples
 #' clear(FALSE) # will not clear memory
