a097cb03d16a2d18c5a7d87d4c2bc391f0334c75
Adam B. Smith <adam.smith@mobot.org>
2022-08-04 23:19:40 -0500

    Change help

--- R/rowColIndexing.r
+++ R/rowColIndexing.r
@@ -2,7 +2,7 @@
 #'
 #' These functions converts index values of cells between row- and column-style indexing of cells in matrices. Column indexing (the default for matrices) has the cell "1" in the upper left corner of the matrix. The cell "2" is below it, and so on. The numbering then wraps around to the top of the next column. Row indexing (the default for rasters, for example), also has cell "1" in the upper left, but cell "2" is to its right, and so on. Numbering then wraps around to the next row.
 #' 
-#' @param x		Either a matrix or a vector with two values, one for the number of rows and one for the number of columns in a matrix.
+#' @param x		Either a matrix, \emph{or} a vector with two values, one for the number of rows and one for the number of columns in a matrix.
 #' @param cell	One or more cell indices (positive integers).
 #' @param dir	The "direction" in which to convert. If \code{'row'}, it is assumed that \code{cell} is a column-style index and so should be converted to a row-style index. If \code{'col'}, it is assumed that \code{cell} is a row-style index and so should be converted to a column-style index.
 #'
